7006a390975c21c1ee121f417e2a2d3c771c5aea
Admin <admin@Tanveers-MacBook-Air.local>
2022-12-26 18:59:37 +0530

    adding python datatypes.

---
+++ datatypes.py
@@ -0,0 +1,20 @@
+# integer
+x = 12
+print(type(x))
+print(x)
+
+# float
+X = 1.2
+print(type(X))
+print(X)
+
+# boolean
+a = 1
+b = 2
+print(type(a<b))
+print (a<b)
+
+#string
+name = "pradeep"
+print(type(name))
+print((name))
